42f247b6f054b36dd6f7c6db614fe6b7ebfc6fa0
Maximchik666 <maximchik666@gmail.com>
2022-07-14 15:18:10 +0300

    Initial Commit

---
+++ gitTrain/gitTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTrainApp.swift
+//  gitTrain
+//
+//  Created by Максим Круглов on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct gitTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
